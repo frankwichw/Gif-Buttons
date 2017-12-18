93a9a76c0996194db317a09cd5445e6edb310444
James Frankwich <jamesfrankwich@Jamess-MacBook-Pro.local>
2017-12-18 13:52:59 -0500

    Final site files

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,39 +15,8 @@ $(document).ready(function(){
 		$("#buttonsGoHere").append(button);
 	}
 
-	// event click listener
-	$("#addAnimalName").click(function(){
-
-		// gets the value of the input from the animalNameSubmit div
-		var buttonValue = $("#animalNameSubmit").val().trim();
-
-		// if/eslse statement compares buttonValue to an empty string
-		if (buttonValue.length < 1){
-
-			// alert to say it's empty
-			alert("Hey!!!!!");
-		} else {
-
-			// else push the buttonValue to the buttonArray
-			buttonArray.push(buttonValue);
-
-			// empty the buttonsGoHere div
-			$("#buttonsGoHere").empty();
-
-			// run four loop that generates buttons
-			for (var i = 0; i < buttonArray.length; i++){
-				var button = $("<button>");
-				button.text(buttonArray[i]);
-				button.attr("data-name", buttonArray[i]);
-				button.addClass("animalButtons");
-				$("#buttonsGoHere").append(button);
-			}
-		}}
-	);
-
 	// event click listener for pressing one of the animal buttons
-	$(".animalButtons").click(function(){
-
+	$(document).on("click", ".animalButtons", function(){
 		// getting the data name in a variable
 		var searchTermV1 = $(this).attr("data-name");
 		console.log(searchTermV1);
@@ -85,11 +54,16 @@ $(document).ready(function(){
 	      		// creating img element in variable
 	      		var gif = $("<img>");
 
-	      		// creating variable to hold url
+	      		// creating variable to hold url and still url
 	      		var imageSRC = response.data[i].images.original.url;
+	      		var imageSRCstill = response.data[i].images.original_still.url;
 
-	      		// adding an attribute to the img element
+	      		// adding an attributes to the img element
 	      		gif.attr("src", imageSRC);
+	      		gif.addClass("gif");
+	      		gif.attr("data-animated", imageSRC);
+	      		gif.attr("data-paused", imageSRCstill);
+	      		gif.attr("data-state", "animated");
 
 	      		// appending image to gifP
 	      		gifP.append(gif);
@@ -99,5 +73,47 @@ $(document).ready(function(){
 	      		$("#gifsGoHere").prepend(ratingInfo);
 	      	}	
       	});
+	});
+
+
+	// event click listener
+	$("#addAnimalName").on("click", function(){
+		
+
+		// gets the value of the input from the animalNameSubmit div
+		var buttonValue = $("#animalNameSubmit").val().trim();
+
+		// if/eslse statement compares buttonValue to an empty string
+		if (buttonValue.length < 1){
+
+			// alert to say it's empty
+			alert("Hey!!!!!");
+		} else {
+
+			// else push the buttonValue to the buttonArray
+			buttonArray.push(buttonValue);
+
+			// empty the buttonsGoHere div
+			$("#buttonsGoHere").empty();
+
+			// run four loop that generates buttons
+			for (var i = 0; i < buttonArray.length; i++){
+				var button = $("<button>");
+				button.text(buttonArray[i]);
+				button.attr("data-name", buttonArray[i]);
+				button.addClass("animalButtons");
+				$("#buttonsGoHere").append(button);
+			}
+		}}
+	);
+
+	$(document).on("click", ".gif", function(){
+		if($(this).attr("data-state") == "animated"){
+			$(this).attr("data-state", "still");
+			$(this).attr("src", $(this).attr("data-paused"));
+		} else {
+			$(this).attr("data-state", "animated");
+			$(this).attr("src", $(this).attr("data-animated"));
+		}
 	});
 });
